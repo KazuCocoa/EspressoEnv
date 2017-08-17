acfe062f10e0ada080cff1ade592a09b0d52dc47
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2017-08-17 11:15:48 +0900

    Update MyScreenshot.kt

--- app/src/androidTest/java/com/example/kazuaki/espressoenv/MyScreenshot.kt
+++ app/src/androidTest/java/com/example/kazuaki/espressoenv/MyScreenshot.kt
@@ -10,6 +10,8 @@ import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
 
+// Some methos related screenshots and test weatchers, inspired by https://github.com/square/spoon
+
 class MyScreenshot {
     private val png: String = ".png"
     private val underscore: String = "_"
