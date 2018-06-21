93f41dcbde93bb7561a56956749eb8b6dba63c18
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2018-06-22 01:51:43 +0900

    remove Espresso.setFailureHandler

--- app/src/androidTest/java/com/example/kazuaki/espressoenv/MyTestWatcher.kt
+++ app/src/androidTest/java/com/example/kazuaki/espressoenv/MyTestWatcher.kt
@@ -1,23 +1,11 @@
 package com.example.kazuaki.espressoenv
 
-import android.support.test.espresso.Espresso
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
-import android.support.test.InstrumentationRegistry
-import android.support.test.espresso.base.DefaultFailureHandler
-import java.util.concurrent.atomic.AtomicBoolean
-
 
 class MyTestWatcher : TestWatcher() {
     override fun failed(e: Throwable, description: Description) {
-        val errorHandled: AtomicBoolean = AtomicBoolean(false)
-
-        Espresso.setFailureHandler { error, viewMatcher ->
-            MyScreenshot().takeScreenshot(description, error.toString())
-            errorHandled.set(true)
-            DefaultFailureHandler(InstrumentationRegistry.getTargetContext()).handle(error, viewMatcher)
-        }
-
+        MyScreenshot().takeScreenshot(description, e.toString())
         super.failed(e, description)
     }
 }
