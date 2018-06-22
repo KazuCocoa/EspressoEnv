3933b8bf8a20f8f8af3e264ceb3c7d0a0900e482
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2018-06-22 23:06:06 +0900

    add clean data

--- app/src/androidTest/java/com/example/kazuaki/espressoenv/MyTestWatcher.kt
+++ app/src/androidTest/java/com/example/kazuaki/espressoenv/MyTestWatcher.kt
@@ -1,11 +1,23 @@
 package com.example.kazuaki.espressoenv
 
+import android.preference.PreferenceManager
+import android.support.test.InstrumentationRegistry
 import org.junit.rules.TestWatcher
 import org.junit.runner.Description
 
 class MyTestWatcher : TestWatcher() {
+    override fun starting(description: Description?) {
+        clearData() // Clear shared preference data in setUp phase
+        super.starting(description)
+    }
+
     override fun failed(e: Throwable, description: Description) {
         MyScreenshot().takeScreenshot(description, e.toString())
         super.failed(e, description)
+    }
+
+    private fun clearData() {
+        val preferenceManager = PreferenceManager.getDefaultSharedPreferences(InstrumentationRegistry.getTargetContext())
+        preferenceManager.edit().clear().commit()
     }
 }
