509239f20d2631341efcd0c07828b748610d811a
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2018-09-22 15:39:45 +0900

    use androidx instead of support libraries except for tests

--- app/src/main/java/com/example/kazuaki/espressoenv/MainActivity.kt
+++ app/src/main/java/com/example/kazuaki/espressoenv/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.example.kazuaki.espressoenv
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 
 import kotlinx.android.synthetic.main.activity_main.*
 
